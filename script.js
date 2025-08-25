2ca5d38d83ec7b2965c2953ec998f3100648dbf4
MrPee309 <linecker309@yahoo.com>
2025-08-25 11:54:23 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('y').textContent = new Date().getFullYear();
-const toggle = document.querySelector('.nav__toggle');
-const nav = document.querySelector('.nav');
-toggle?.addEventListener('click', () => {
-  nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-});
